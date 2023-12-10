0b513212182c214659e1f7599d0876b23e7088ae
Constantin Clerc <cclerc@florimont.ch>
2023-12-10 22:06:59 +0100

    Initial Commit

---
+++ Geranium/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Geranium
+//
+//  Created by Constantin Clerc on 10/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Geranium/GeraniumApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeraniumApp.swift
+//  Geranium
+//
+//  Created by Constantin Clerc on 10/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GeraniumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
