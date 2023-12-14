dedd37e54954c658ea404f769c3e57a910bdf29b
Constantin Clerc <cclerc@florimont.ch>
2023-12-14 17:07:19 +0100

    Periodic refresh

---
+++ Geranium/Libs/userspaceribout.h
@@ -0,0 +1,13 @@
+//
+//  userspaceribout.h
+//  Geranium
+//
+//  Created by cclerc on 14.12.23.
+//
+
+#ifndef userspaceribout_h
+#define userspaceribout_h
+
+#include <stdio.h>
+
+#endif /* userspaceribout_h */
