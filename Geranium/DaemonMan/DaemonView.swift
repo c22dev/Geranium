86e152a8bfdeff8b91b77ea592fe2da6503a4e75
Constantin Clerc <cclerc@florimont.ch>
2023-12-14 16:35:51 +0100

    Added kill process

--- Geranium/DaemonMan/DaemonView.swift
+++ Geranium/DaemonMan/DaemonView.swift
@@ -18,24 +18,34 @@ struct DaemonView: View {
 
     init() {
         let rawProcesses = sysctl_ps()
-        self.processes = rawProcesses!.compactMap { rawProcess in
+        self.processes = rawProcesses?.compactMap { rawProcess in
             guard let dict = rawProcess as? NSDictionary,
                   let pid = dict["pid"] as? String,
                   let procName = dict["proc_name"] as? String else {
                 return nil
             }
             return ProcessItem(pid: pid, procName: procName)
-        }
+        } ?? []
     }
 
     var body: some View {
-        List(processes) { process in
-            HStack {
-                Text("PID: \(process.pid)")
-                Spacer()
-                Text("Name: \(process.procName)")
+        NavigationView {
+            List {
+                ForEach(processes) { process in
+                    HStack {
+                        Text("PID: \(process.pid)")
+                        Spacer()
+                        Text("Name: \(process.procName)")
+                    }
+                }
+                .onDelete { indexSet in
+                    guard let index = indexSet.first else { return }
+                    let process = processes[index]
+                    killall(process.procName)
+                    //TODO : FETCH AFTER DELETE
+                }
             }
+            .navigationTitle("Processes")
         }
-        .navigationTitle("Processes")
     }
 }
