88db872c47806db35277c0c97403cc810d09b943
c22dev <102235607+c22dev@users.noreply.github.com>
2023-12-14 14:34:16 +0100

    Delete build directory

--- build/DerivedData/Build/Intermediates.noindex/Geranium.build/Debug-iphoneos/Geranium.build/Objects-normal/arm64/TSUtil.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/etudiant/DocumentsAlt/Xcode/Geranium/Geranium/Libs/TrollStore/TSUtil.m \
-  /Users/etudiant/DocumentsAlt/Xcode/Geranium/Geranium/Libs/TrollStore/TSUtil.h \
-  /Users/etudiant/DocumentsAlt/Xcode/Geranium/Geranium/Libs/TrollStore/CoreServices.h
