4eaaad8660e4e9396ed456246d71d8b5acb7fb3b
Constantin Clerc <cclerc@florimont.ch>
2023-12-19 15:14:32 +0100

    welcome page

---
+++ Geranium/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  Geranium
+//
+//  Created by cclerc on 19.12.23.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
