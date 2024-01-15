3e1e9ffffd554d166da85d27415e8b72ac9ca28a
Constantin Clerc <cclerc@florimont.ch>
2024-01-16 00:23:49 +0100

    help me pls

--- Geranium/DaemonMan/CurrentlyDisabledDaemonView.swift
+++ Geranium/DaemonMan/CurrentlyDisabledDaemonView.swift
@@ -59,8 +59,8 @@ struct CurrentlyDisabledDaemonView: View {
     private func saveIt() {
         (data as NSDictionary).write(toFile: plistPath, atomically: true)
         print(result)
-        result = RootHelper.move(from: URL(fileURLWithPath: "/var/mobile/Documents/disabled.plist"), to: URL(fileURLWithPath: "/var/db/com.apple.xpc.launchd/disabled.plist"))
         var result = RootHelper.removeItem(at: URL(fileURLWithPath: "/var/db/com.apple.xpc.launchd/disabled.plist"))
+        result = RootHelper.move(from: URL(fileURLWithPath: "/var/mobile/Documents/disabled.plist"), to: URL(fileURLWithPath: "/var/db/com.apple.xpc.launchd/disabled.plist"))
         print(result)
     }
     
